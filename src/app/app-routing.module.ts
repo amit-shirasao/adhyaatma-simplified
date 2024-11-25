4e921ad233c5e49dbc76efc8216ddfd31c1302f9
Amit Shirasao <amit.shirasao@gmail.com>
2024-11-25 09:10:16 +0530

    Default route: Ganesh Aarti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'ganapati-aarti',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
-    redirectTo: 'ganapati-aarti',
-    pathMatch: 'full'
-  },
-  {
-    path: 'ganapati-aarti',
     loadChildren: () => import('./pages/ganapati-aarti/ganapati-aarti.module').then(m => m.GanapatiAartiPageModule)
   },
   {
