702298ac2afc24c1de24af8379f8b420a23a98a7
Amit Shirasao <amit.shirasao@gmail.com>
2024-11-27 16:59:46 +0530

    Dynamic Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Share } from '@capacitor/share';
 import { MenuController } from '@ionic/angular';
 @Component({
@@ -7,8 +9,24 @@ import { MenuController } from '@ionic/angular';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(private menuController: MenuController, private location: Location) {
+export class AppComponent implements OnInit {
+  public pageId: string = '';
+  @ViewChild('dynamicContent') dynamicContent!: ElementRef;
+
+  constructor(private menuController: MenuController,
+    private location: Location,
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.pageId = this.router.url.slice(1) || 'landing-page';
+        this.loadContent(this.pageId);
+      }
+    });
   }
 
   closeTheMenu() {
@@ -21,5 +39,12 @@ export class AppComponent {
       text: 'Check out this aswesome Hindu scripture:',
       url: this.location.path()
     });
+  }
+
+  loadContent(pageId: string) {
+    this.http.get(`assets/pages/${pageId}.html`, { responseType: 'text' })
+      .subscribe(html => {
+        this.dynamicContent.nativeElement.innerHTML = html;
+      });
   }
 }
--- src/app/pipes/new-line-characters.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewLineCharactersPipe } from './new-line-characters.pipe';
-
-describe('NewLineCharactersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NewLineCharactersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
