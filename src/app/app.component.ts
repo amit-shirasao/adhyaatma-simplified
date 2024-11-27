f863072e43d46d4c4d1f16a4d99fae5e8eeb30a5
Amit Shirasao <amit.shirasao@gmail.com>
2024-11-27 09:58:38 +0530

    'Share' feature is implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Share } from '@capacitor/share';
 import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
@@ -6,10 +8,18 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private menuController: MenuController) {
+  constructor(private menuController: MenuController, private location: Location) {
   }
 
   closeTheMenu() {
     this.menuController.close();
+  }
+
+  async share() {
+    await Share.share({
+      title: 'अध्यात्म',
+      text: 'Check out this aswesome Hindu scripture:',
+      url: this.location.path()
+    });
   }
 }
